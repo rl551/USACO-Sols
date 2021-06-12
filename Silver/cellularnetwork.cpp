1e4db4b67ab6fd97756bfbf4c4bd4069f016d69e
rl551 <73052620+rl551@users.noreply.github.com>
2021-06-12 14:10:05 -0700

    Update cellularnetwork.cpp

--- Silver/cellularnetwork.cpp
+++ Silver/cellularnetwork.cpp
@@ -1,22 +1,7 @@
 //https://codeforces.com/contest/702/problem/C
 #include "bits/stdc++.h"
 #define ll long long
-#define str string
-#define db double
-#define pb push_back
-#define mp make_pair
-#define pi pair<int, int>
-#define ps pair<str, str>
-#define pdb pair<db, db>
-#define pll pair<ll, ll>
-#define vi vector<int>
-#define vs vector<str>
-#define vdb vector<db>
 #define vll vector<ll>
-#define vpi vector<pi>
-#define vps vector<ps>
-#define vpdb vector<pdb>
-#define vpll vector<pll>
 using namespace std;
 
 ll N, M;
