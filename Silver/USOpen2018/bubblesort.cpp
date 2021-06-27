56ff7d03f3195dfb2cbd78843127748ec694ee7b
rl551 <73052620+rl551@users.noreply.github.com>
2021-06-27 10:59:14 -0700

    Create bubblesort.cpp

---
+++ Silver/USOpen2018/bubblesort.cpp
@@ -0,0 +1,36 @@
+#include "bits/stdc++.h"
+#define pb push_back
+#define mp make_pair
+#define f first
+#define s second
+using namespace std;
+
+int N, ans = -1;
+vector<pair<int, int> > A;
+
+bool comp(pair<int, int>&a, pair<int, int>&b){
+    if(a.f != b.f){
+        return a.f<b.f;
+    } else{
+        return a.s<b.s;
+    }
+}
+
+int main(){
+
+    ifstream fin("sort.in");
+    ofstream fout("sort.out");
+
+    fin >> N;
+    for(int i=0; i<N; ++i){
+        int a; fin >> a;
+        A.pb(mp(a,i));
+    }
+    sort(A.begin(), A.end(), comp);
+    for(int i=0; i<N; ++i){
+        ans = max(ans, abs(A[i].s-i));
+    }
+    fout << ans+1;
+
+    return 0;
+}
