19ea4b6476bd60f88354685fbd88ecc66a78e807
rl551 <73052620+rl551@users.noreply.github.com>
2021-07-01 23:36:23 -0700

    Update moocast.cpp

--- Silver/Dec2016/moocast.cpp
+++ Silver/Dec2016/moocast.cpp
@@ -1,23 +1,16 @@
 #include "bits/stdc++.h"
-#define pb push_back
-#define mp make_pair
-#define f first
-#define s second
 using namespace std;
 
-int N, ans=0, counter=0;
+int N, counter = 0, ans = -1;
 vector<pair<pair<int, int>, int> > cows;
-vector<bool> visited(200);
-vector<vector<int> > adj(200);
+vector<int> visited(200);
 
 void dfs(int start){
-    if(visited[start]){
-        return;
-    }
+    if(visited[start]){return;}
     visited[start] = true;
-    for(auto &u: adj[start]){
-        if(!visited[u]){
-            dfs(u);
+    for(int j=0; j<N; ++j){
+        if(!visited[j] && sqrt(pow(cows[j].first.first-cows[start].first.first,2)+pow(cows[j].first.second-cows[start].first.second,2)) <= cows[start].second){
+            dfs(j);
         }
     }
 }
@@ -26,37 +19,21 @@ int main(){
 
     ifstream fin("moocast.in");
     ofstream fout("moocast.out");
-
     fin >> N;
     for(int i=0; i<N; ++i){
-        int x, y, p;
-        fin >> x >> y >> p;
-        cows.pb(mp(mp(x,y),p));
+        int x, y, p; fin >> x >> y >> p;
+        cows.push_back(make_pair(make_pair(x,y),p));
     }
     for(int i=0; i<N; ++i){
-        for(int j=i+1; j<N; ++j){
-            if(pow(cows[i].f.f-cows[j].f.f,2)+pow(cows[i].f.s-cows[j].f.s,2) <= pow(cows[i].s,2)){
-                adj[i].pb(j);
-            }
-            if(pow(cows[i].f.f-cows[j].f.f,2)+pow(cows[i].f.s-cows[j].f.s,2) <= pow(cows[j].s,2)){
-                adj[j].pb(i);
-            }
-        }
-    }
-    for(int i=0; i<N; ++i){
-        counter = 0;
         fill(visited.begin(), visited.end(), false);
         dfs(i);
+        counter = 0;
         for(int j=0; j<N; ++j){
-            if(visited[j]){
-                counter++;
-            }
+            if(visited[j]){counter++;}
         }
-        if(counter>ans){
-            ans=counter;
-        }
+        if(counter>ans){ans=counter;}
     }
     fout << ans;
-
+    
     return 0;
 }
