dfec016d9bbea150442b441c61cba2f7c38f0dea
rl551 <73052620+rl551@users.noreply.github.com>
2021-07-05 15:29:03 -0700

    Update moocast.cpp

--- Silver/Dec2016/moocast.cpp
+++ Silver/Dec2016/moocast.cpp
@@ -1,39 +1,50 @@
 #include "bits/stdc++.h"
+#define f first
+#define s second
 using namespace std;
 
-int N, counter = 0, ans = -1;
-vector<pair<pair<int, int>, int> > cows;
-vector<int> visited(200);
+int N, counter;
+vector<pair<int, int> > cows;
+vector<bool> visited(1e3);
 
-void dfs(int start){
-    if(visited[start]){return;}
+void dfs(int start, int X){
+    if(visited[start]){
+        return;
+    }
     visited[start] = true;
-    for(int j=0; j<N; ++j){
-        if(!visited[j] && sqrt(pow(cows[j].first.first-cows[start].first.first,2)+pow(cows[j].first.second-cows[start].first.second,2)) <= cows[start].second){
-            dfs(j);
+    counter++;
+    for(int i=0; i<N; ++i){
+        if(i != start && !visited[i] && (pow(cows[start].f-cows[i].f,2)+pow(cows[start].s-cows[i].s,2) <= X)){
+            dfs(i, X);
         }
     }
 }
+bool check(int X){
+    counter = 0;
+    fill(visited.begin(), visited.end(), false);
+    dfs(0,X);
+    return(!(counter<N));
+}
 
 int main(){
-
     ifstream fin("moocast.in");
     ofstream fout("moocast.out");
     fin >> N;
     for(int i=0; i<N; ++i){
-        int x, y, p; fin >> x >> y >> p;
-        cows.push_back(make_pair(make_pair(x,y),p));
+        int x, y; fin >> x >> y;
+        cows.push_back(make_pair(x,y));
     }
-    for(int i=0; i<N; ++i){
-        fill(visited.begin(), visited.end(), false);
-        dfs(i);
-        counter = 0;
-        for(int j=0; j<N; ++j){
-            if(visited[j]){counter++;}
+    int l=1, h=1e9, m=(l+h)/2;
+    while(l<=h){
+        if(check(m) && !check(m-1)){
+            fout << m;
+            return 0;
+        } else if(!check(m)){
+            l=m+1;
+            m=(l+h)/2;
+        } else{
+            h=m-1;
+            m=(l+h)/2;
         }
-        if(counter>ans){ans=counter;}
     }
-    fout << ans;
-    
-    return 0;
 }
