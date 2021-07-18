14cb615505059aabd69da3581a908f83ce8239ec
rl551 <73052620+rl551@users.noreply.github.com>
2021-07-17 22:20:37 -0700

    Update ccski.cpp

--- Silver/Jan2014/ccski.cpp
+++ Silver/Jan2014/ccski.cpp
@@ -1,20 +1,20 @@
 #include "bits/stdc++.h"
-const long MN=500;
-const long long X_CHANGE[]={1, 0, -1, 0};
-const long long Y_CHANGE[]={0, -1, 0, 1};
+const int NM=500;
+const int X_CHANGE[]={1, 0, -1, 0};
+const int Y_CHANGE[]={0, -1, 0, 1};
 using namespace std;
 
-long long M, N, course[MN][MN];
-bool visited[MN][MN], waypoint[MN][MN];
+int M, N, course[NM][NM];
+bool visited[NM][NM], waypoint[NM][NM];
 
-void floodfill(long long i, long long j, long long D){
+void floodfill(int i, int j, int D){
     if(visited[i][j] || (i<0 || i>=M || j<0 || j>=N)) return;
     visited[i][j] = true;
     for(int k=0; k<4; ++k){
         if(abs(course[i+X_CHANGE[k]][j+Y_CHANGE[k]]-course[i][j]) <= D) floodfill(i+X_CHANGE[k], j+Y_CHANGE[k], D);
     }
 }
-bool check(long long D){
+bool check(int D){
     for(int i=0; i<M; ++i){
         for(int j=0; j<N; ++j){
             visited[i][j] = false;
@@ -39,12 +39,12 @@ int main(){
     for(int i=0; i<M; ++i){
         for(int j=0; j<N; ++j) fin >> waypoint[i][j];
     }
-    long long lo = 0, hi = 1e9, best;
+    int lo = 0, hi = 1e9, best;
     while (lo <= hi) {
-        long long mid = (lo+hi)/2;
+        int mid = (lo + hi) / 2;
         if (check(mid)) {
             best = mid;
-            hi = mid-1;
+            hi = mid - 1;
         } else lo=mid+1;
     }
     fout << best;
