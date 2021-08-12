c706b0ee1868408ca206eb269d644b3fbabde60f
rl551 <73052620+rl551@users.noreply.github.com>
2021-08-12 15:10:20 -0700

    Update haybales.cpp

--- Silver/Dec2016/haybales.cpp
+++ Silver/Dec2016/haybales.cpp
@@ -1,22 +1,28 @@
 #include "bits/stdc++.h"
-#define pb push_back
 using namespace std;
 
-int N, Q, A, B;
+int N, Q;
 vector<int> haybales;
 
 int main(){
 
-    cin >> N >> Q;
+    ifstream fin("haybales.in");
+    ofstream fout("haybales.out");
+    fin >> N >> Q;
     for(int i=0; i<N; ++i){
-        int h; cin >> h;
-        haybales.pb(h);
+        int h; fin >> h;
+        haybales.push_back(h);
     }
     sort(haybales.begin(), haybales.end());
     for(int i=0; i<Q; ++i){
-        cin >> A >> B;        
-        cout << upper_bound(haybales.begin(), haybales.end(), B)-lower_bound(haybales.begin(), haybales.end(), A) << endl;
+        int a,b; fin >> a >> b;
+        int l=lower_bound(haybales.begin(), haybales.end(), a)-haybales.begin();
+        int r=upper_bound(haybales.begin(), haybales.end(), b)-haybales.begin();
+        //a <= a_1, a_2, ..., a_k <= b < b_1, b_2, ...
+        //*l=a_1
+        //*r=b_1
+        fout << r-l << endl;
     }
-    
+
     return 0;
 }
