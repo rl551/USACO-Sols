52a6549fa85a547bfd887530c9305a0069955c67
rl551 <73052620+rl551@users.noreply.github.com>
2021-08-12 22:41:05 -0700

    Update angrycows.cpp

--- Silver/Jan2016/angrycows.cpp
+++ Silver/Jan2016/angrycows.cpp
@@ -1,54 +1,39 @@
-//not correct on tc4
 #include "bits/stdc++.h"
-#define pb push_back
 using namespace std;
 
 int N, K;
-vector<int> cows;
+vector<int> haybales;
 
 bool check(int R){
-    int curpos = 0;
+    int curindex=-1;
     for(int i=0; i<K; ++i){
-        int l=curpos, h=N-1, m = (l+h)/2;
-        while(l <= h){
-            if(cows[m] <= cows[curpos]+2*R && (m == N-1 || cows[m+1]>cows[curpos]+2*R)){
-                curpos = m+1;
-                break;
-            } else if(cows[m] > cows[curpos]+2*R){
-                h = m-1;
-                m = (l+h)/2;
-            } else{
-                l = m+1;
-                m = (l+h)/2;
-            }
-        }
+        curindex++;
+        auto nextindex=upper_bound(haybales.begin(), haybales.end(), haybales[curindex]+2*R); nextindex--;
+        curindex = nextindex-haybales.begin();
+        if(curindex >= N-1) return true;
     }
-    return(curpos >= N-1);
+    return(curindex >= N-1);
 }
 
 int main(){
 
     ifstream fin("angry.in");
     ofstream fout("angry.out");
-
     fin >> N >> K;
     for(int i=0; i<N; ++i){
-        int x; fin >> x;
-        cows.pb(x);
+        int x_i; fin >> x_i;
+        haybales.push_back(x_i);
     }
-    sort(cows.begin(), cows.end());
-    int low = 0, high = pow(10,9), mid = (low+high)/2;
-    while(low <= high){
-        if(check(mid) && (mid == 0 || !check(mid-1))){
-            fout << mid;
-            return 0;
-        } else if(!check(mid)){
-            low = mid+1;
-            mid = (low+high)/2;
-        } else{
-            high = mid-1;
-            mid = (low+high)/2;
-        }
+    sort(haybales.begin(), haybales.end());
+    int lo=0, hi=1e9, ans;
+    while(lo<=hi){//false, false, ..., false, true, true, true, ...
+        int mi=(lo+hi)/2;
+        if(check(mi)){
+            ans=mi;
+            hi=mi-1;
+        } else lo=mi+1;
     }
+    fout << ans;
 
+    return 0;
 }
